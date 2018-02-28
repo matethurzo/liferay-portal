63b20730c1ba67a2b2d197ffbabc53d7f806085a
akosthurzo <akos.thurzo@liferay.com>
2018-02-28 10:13:13 +0100

    LPS-79230 Add logic to store publication info

--- modules/apps/web-experience/export-import/export-import-api/src/main/java/com/liferay/exportimport/data/handler/base/BaseStagedModelDataHandler.java
+++ modules/apps/web-experience/export-import/export-import-api/src/main/java/com/liferay/exportimport/data/handler/base/BaseStagedModelDataHandler.java
@@ -18,14 +18,28 @@ import aQute.bnd.annotation.ProviderType;
 
 import com.liferay.exportimport.content.processor.ExportImportContentProcessor;
 import com.liferay.exportimport.content.processor.ExportImportContentProcessorRegistryUtil;
+import com.liferay.exportimport.kernel.lar.ExportImportProcessCallbackRegistryUtil;
+import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataException;
+import com.liferay.exportimport.kernel.lar.PortletDataHandlerKeys;
+import com.liferay.exportimport.service.EntityPublicationLocalServiceUtil;
 import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.portal.background.task.model.BackgroundTask;
+import com.liferay.portal.background.task.service.BackgroundTaskLocalServiceUtil;
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.DateRange;
+import com.liferay.portal.kernel.util.MapUtil;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 /**
  * @author Daniel Kocsis
@@ -68,6 +82,20 @@ public abstract class BaseStagedModelDataHandler<T extends StagedModel>
 		throws PortletDataException {
 
 		super.exportStagedModel(portletDataContext, stagedModel);
+
+		boolean updateLastPublishDate = MapUtil.getBoolean(
+			portletDataContext.getParameterMap(),
+			PortletDataHandlerKeys.UPDATE_LAST_PUBLISH_DATE);
+
+		if (ExportImportThreadLocal.isStagingInProcess() &&
+			updateLastPublishDate &&
+			!ExportImportThreadLocal.isInitialLayoutStagingInProcess()) {
+
+			ExportImportProcessCallbackRegistryUtil.registerCallback(
+				portletDataContext.getExportImportProcessId(),
+				new UpdateStagedModelLastPublishDateCallable(
+					stagedModel, portletDataContext.getDateRange()));
+		}
 	}
 
 	@Override
@@ -140,6 +168,57 @@ public abstract class BaseStagedModelDataHandler<T extends StagedModel>
 
 	protected StagedModelRepository<T> getStagedModelRepository() {
 		return null;
+	}
+
+	private class UpdateStagedModelLastPublishDateCallable
+		implements Callable<Void> {
+
+		public UpdateStagedModelLastPublishDateCallable(
+			T stagedModel, DateRange dateRange) {
+
+			_classNameId = ClassNameLocalServiceUtil.getClassNameId(
+				stagedModel.getModelClassName());
+			_classPK = (long)stagedModel.getPrimaryKeyObj();
+			_dateRange = dateRange;
+		}
+
+		@Override
+		public Void call() throws PortalException {
+			Date publicationDate = null;
+
+			if (_dateRange != null) {
+				publicationDate = _dateRange.getEndDate();
+			}
+
+			if ((publicationDate == null) &&
+				BackgroundTaskThreadLocal.hasBackgroundTask()) {
+
+				long backgroundTaskId =
+					BackgroundTaskThreadLocal.getBackgroundTaskId();
+
+				BackgroundTask backgroundTask =
+					BackgroundTaskLocalServiceUtil.fetchBackgroundTask(
+						backgroundTaskId);
+
+				if (backgroundTask != null) {
+					publicationDate = backgroundTask.getCreateDate();
+				}
+			}
+
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			EntityPublicationLocalServiceUtil.updateEntityPublication(
+				serviceContext.getUserId(), _classNameId, _classPK,
+				publicationDate);
+
+			return null;
+		}
+
+		private final long _classNameId;
+		private final long _classPK;
+		private final DateRange _dateRange;
+
 	}
 
 }
