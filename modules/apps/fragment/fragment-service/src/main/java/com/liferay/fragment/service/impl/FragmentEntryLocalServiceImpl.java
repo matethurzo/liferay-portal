f882ac30bd39ac44377388986c6247995bb0c011
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-06-18 16:38:33 +0200

    LPS-82572 Use only one method for adding fragments, maintaining the optimal option

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLocalServiceImpl.java
@@ -89,45 +89,9 @@ public class FragmentEntryLocalServiceImpl
 			int status, ServiceContext serviceContext)
 		throws PortalException {
 
-		// Fragment entry
-
-		User user = userLocalService.getUser(userId);
-
-		validate(name);
-
-		if (Validator.isNull(fragmentEntryKey)) {
-			fragmentEntryKey = _generateFragmentEntryKey(groupId, name);
-		}
-
-		fragmentEntryKey = _getFragmentEntryKey(fragmentEntryKey);
-
-		validateFragmentEntryKey(groupId, fragmentEntryKey);
-
-		long fragmentEntryId = counterLocalService.increment();
-
-		FragmentEntry fragmentEntry = fragmentEntryPersistence.create(
-			fragmentEntryId);
-
-		fragmentEntry.setUuid(serviceContext.getUuid());
-		fragmentEntry.setGroupId(groupId);
-		fragmentEntry.setCompanyId(user.getCompanyId());
-		fragmentEntry.setUserId(user.getUserId());
-		fragmentEntry.setUserName(user.getFullName());
-		fragmentEntry.setCreateDate(serviceContext.getCreateDate(new Date()));
-		fragmentEntry.setModifiedDate(
-			serviceContext.getModifiedDate(new Date()));
-		fragmentEntry.setFragmentCollectionId(fragmentCollectionId);
-		fragmentEntry.setFragmentEntryKey(fragmentEntryKey);
-		fragmentEntry.setName(name);
-		fragmentEntry.setPreviewFileEntryId(previewFileEntryId);
-		fragmentEntry.setStatus(status);
-		fragmentEntry.setStatusByUserId(userId);
-		fragmentEntry.setStatusByUserName(user.getFullName());
-		fragmentEntry.setStatusDate(new Date());
-
-		fragmentEntryPersistence.update(fragmentEntry);
-
-		return fragmentEntry;
+		return addFragmentEntry(
+			userId, groupId, fragmentCollectionId, fragmentEntryKey, name, null,
+			null, null, status, serviceContext);
 	}
 
 	@Override
@@ -178,14 +142,14 @@ public class FragmentEntryLocalServiceImpl
 
 		User user = userLocalService.getUser(userId);
 
+		validate(name);
+
 		if (Validator.isNull(fragmentEntryKey)) {
-			fragmentEntryKey = String.valueOf(counterLocalService.increment());
-		}
-		else {
-			fragmentEntryKey = _getFragmentEntryKey(fragmentEntryKey);
+			fragmentEntryKey = _generateFragmentEntryKey(groupId, name);
 		}
 
-		validate(name);
+		fragmentEntryKey = _getFragmentEntryKey(fragmentEntryKey);
+
 		validateFragmentEntryKey(groupId, fragmentEntryKey);
 
 		if (WorkflowConstants.STATUS_APPROVED == status) {
@@ -199,6 +163,7 @@ public class FragmentEntryLocalServiceImpl
 		FragmentEntry fragmentEntry = fragmentEntryPersistence.create(
 			fragmentEntryId);
 
+		fragmentEntry.setUuid(serviceContext.getUuid());
 		fragmentEntry.setGroupId(groupId);
 		fragmentEntry.setCompanyId(user.getCompanyId());
 		fragmentEntry.setUserId(user.getUserId());
