951430752148a56cca37b854f6c081101e592cd6
Daniel Kocsis <daniel.kocsis@liferay.com>
2018-11-26 09:44:12 +0100

    LPS-87540 Autogenerated

--- modules/apps/change-tracking/change-tracking-engine-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/change-tracking/change-tracking-engine-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,9 +1,5 @@
 create index IX_E6FA8A87 on CTEChangeEntry (resourcePrimKey);
 
-create index IX_A4F16793 on ChangeEntry (resourcePrimKey);
-
-create index IX_548A249 on Collections_Entries (changeCollectionId);
-create index IX_DD667D61 on Collections_Entries (changeEntryId);
 create index IX_8745ABC on Collections_Entries (companyId);
 create index IX_5F525961 on Collections_Entries (cteChangeCollectionId);
 create index IX_493CB749 on Collections_Entries (cteChangeEntryId);
--- modules/apps/change-tracking/change-tracking-engine-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/change-tracking/change-tracking-engine-service/src/main/resources/META-INF/sql/tables.sql
@@ -21,29 +21,6 @@ create table CTEChangeEntry (
 	resourcePrimKey LONG
 );
 
-create table ChangeCollection (
-	changeCollectionId LONG not null primary key,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	name VARCHAR(75) null,
-	description VARCHAR(75) null
-);
-
-create table ChangeEntry (
-	changeEntryId LONG not null primary key,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	classNameId LONG,
-	classPK LONG,
-	resourcePrimKey LONG
-);
-
 create table Collections_Entries (
 	companyId LONG not null,
 	cteChangeCollectionId LONG not null,
