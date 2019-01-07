63200491cd5731d7203d8cdfecc2b37050a0ba00
Norbert Nemeth <norbert.nemeth@liferay.com>
2019-01-07 08:06:14 +0100

    init3
    use soy component renderer

---
+++ modules/apps/change-tracking/change-tracking-change-lists-configuration-web/src/main/resources/META-INF/resources/js/ChangeListConfiguration.es.js
@@ -0,0 +1,22 @@
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+
+import templates from './ChangeListConfiguration.soy';
+
+/**
+ * ChangeListConfiguration
+ *
+ */
+
+class ChangeListConfiguration extends Component {
+
+}
+
+ChangeListConfiguration.STATE = {
+
+
+};
+
+Soy.register(ChangeListConfiguration, templates);
+
+export default ChangeListConfiguration;
