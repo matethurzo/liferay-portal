5c767dd508a31d57f0fd69146505437d9f795232
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-01-17 16:48:12 +0100

    LPS-89170 Running 'ant format-source' on the generated code

--- portal-impl/src/com/liferay/portal/model/impl/LayoutSetVersionImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutSetVersionImpl.java
@@ -27,11 +27,13 @@ import aQute.bnd.annotation.ProviderType;
  */
 @ProviderType
 public class LayoutSetVersionImpl extends LayoutSetVersionBaseImpl {
-	/*
+
+	/**
 	 * NOTE FOR DEVELOPERS:
 	 *
 	 * Never reference this class directly. All methods that expect a layout set version model instance should use the {@link com.liferay.portal.kernel.model.LayoutSetVersion} interface instead.
 	 */
 	public LayoutSetVersionImpl() {
 	}
+
 }
