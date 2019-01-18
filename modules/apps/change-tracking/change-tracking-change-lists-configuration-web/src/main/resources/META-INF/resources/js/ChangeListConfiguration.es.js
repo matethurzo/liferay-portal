4f340bb336b79de8e2432486533fd04dc0130ff8
Máté Thurzó <mate.thurzo@liferay.com>
2019-01-18 17:36:21 +0100

    UI

---
+++ modules/apps/change-tracking/change-tracking-change-lists-configuration-web/src/main/resources/META-INF/resources/js/ChangeListConfiguration.es.js
@@ -0,0 +1,225 @@
+import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
+import Soy from 'metal-soy';
+import {Config} from 'metal-state';
+import {openToast} from 'frontend-js-web/liferay/toast/commands/OpenToast.es';
+
+import templates from './ChangeListConfiguration.soy';
+
+/**
+ * Turns Change Lists on/off
+ * ...
+ */
+class ChangeListConfiguration extends PortletBase {
+
+	created() {
+		this._getDataRequest(
+			this.urlChangeListConfigApi,
+			response => {
+				if (response) {
+					this.setState(
+						{
+							changeTrackingEnabled: response.changeTrackingEnabled,
+							initFetch: true,
+							tooltipBody: response.supportedContentTypes
+						}
+					);
+				}
+			}
+		);
+	}
+
+	handleCheck(event) {
+		this.setState(
+			{changeTrackingEnabled: event.target.checked}
+		);
+	}
+
+	save(event) {
+		event.preventDefault();
+
+		let data = {
+			changeTrackingEnabled: this.changeTrackingEnabled
+		};
+
+		this._putDataRequest(
+			this.urlChangeListConfigApi,
+			data,
+			response => {
+
+				// TODO open toast success
+
+				const message = 'saved!';
+
+				openToast(
+					{
+						message,
+						title: Liferay.Language.get('success'),
+						type: 'success'
+					}
+				);
+			}
+		);
+	}
+
+	saveAndGoToOverview(event) {
+		let data = {
+			changeTrackingEnabled: this.changeTrackingEnabled
+		};
+
+		this._putDataRequest(
+			this.urlChangeListConfigApi,
+			data,
+			response => {
+				if (response) {
+
+					// TODO redirect to overview
+
+					const message = 'saved and navigate!';
+
+					openToast(
+						{
+							message,
+							title: Liferay.Language.get('success'),
+							type: 'success'
+						}
+					);
+				}
+			}
+		);
+	}
+
+	_getDataRequest(url, callback) {
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+
+		const request = {
+			credentials: 'include',
+			headers,
+			method: 'GET'
+		};
+
+		fetch(url, request)
+			.then(
+				response => response.json()
+			)
+			.then(
+				response => {
+					callback(response);
+				}
+			)
+			.catch(
+				(error) => {
+
+					// TODO open toast error
+
+					const message = typeof error === 'string' ?
+						error :
+						Liferay.Language.get('error');
+
+					openToast(
+						{
+							message,
+							title: Liferay.Language.get('error'),
+							type: 'danger'
+						}
+					);
+				}
+			);
+	}
+	_putDataRequest(url, bodyData, callback) {
+		let body = JSON.stringify(bodyData);
+
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+
+		const request = {
+			body,
+			credentials: 'include',
+			headers,
+			method: 'PUT'
+		};
+
+		fetch(url, request)
+			.then(
+				response => response.json()
+			)
+			.then(
+				response => {
+					callback(response);
+				}
+			)
+			.catch(
+				(error) => {
+
+					// TODO open toast error
+
+					const message = typeof error === 'string' ?
+						error :
+						Liferay.Language.get('error');
+
+					openToast(
+						{
+							message,
+							title: Liferay.Language.get('error'),
+							type: 'danger'
+						}
+					);
+				}
+			);
+	}
+}
+
+/**
+ * State definition.
+ *
+ * @static
+ * @type {!Object}
+ */
+ChangeListConfiguration.STATE = {
+
+	/**
+	 * Initial fetch happend?
+	 *
+	 * @type {Boolean}
+	 */
+	initFetch: Config.bool().value(false),
+
+	/**
+	 * api url
+	 *
+	 * @type {String}
+	 */
+	urlChangeListConfigApi: Config.string().required(),
+
+	/**
+	 * change tracking on/off
+	 *
+	 * @type {Boolean}
+	 */
+	changeTrackingEnabled: Config.bool(),
+
+	/**
+	 * PortalURL
+	 *
+	 * @type {String}
+	 */
+	portalURL: Config.string().required(),
+
+	/**
+	 * Path to images.
+	 *
+	 * @type {String}
+	 */
+	spritemap: Config.string().required(),
+
+	/**
+	 * Lists of supported content types that are used up in tooltip
+	 *
+	 * @type {List<String>}
+	 */
+	tooltipBody: Config.array()
+};
+
+Soy.register(ChangeListConfiguration, templates);
+
+export default ChangeListConfiguration;
