7e6212354b08cff92b5a1cacad58b54fa5e3df6e
David Varga <david.varga@liferay.com>
2019-01-18 16:20:12 +0100

    LPS-89110 Create test for rest service turning on and off change tracking

---
+++ modules/apps/change-tracking/change-tracking-client-test/src/testIntegration/java/com/liferay/change/tracking/client/test/ChangeTrackingRestTest.java
@@ -0,0 +1,211 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.client.test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.isOneOf;
+
+import com.liferay.change.tracking.client.test.internal.activator.ChangeTrackingDisabledRestTestActivator;
+import com.liferay.oauth2.provider.test.util.OAuth2ProviderTestUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import java.net.URL;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpStatus;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Daniel Kocsis
+ */
+@RunAsClient
+@RunWith(Arquillian.class)
+public class ChangeTrackingRestTest {
+
+	@Deployment
+	public static Archive<?> getArchive() throws Exception {
+		return OAuth2ProviderTestUtil.getArchive(
+			ChangeTrackingDisabledRestTestActivator.class);
+	}
+
+	@Before
+	public void setUp() {
+		RestAssured.port = 8080;
+		RestAssured.basePath = "/o/change-tracking";
+		RestAssured.baseURI = _url.toExternalForm();
+		RestAssured.authentication = RestAssured.preemptive(
+		).basic(
+			"test@liferay.com", "test"
+		);
+
+		_companyId = RestAssured.get(
+			"configurations/"
+		).then(
+		).extract(
+		).jsonPath(
+		).getLong(
+			"[0].companyId"
+		);
+	}
+
+	@Test
+	public void testGetCTConfigurations() throws PortalException {
+		RestAssured.get(
+			"configurations/"
+		).then(
+		).statusCode(
+			HttpStatus.SC_OK
+		).body(
+			"[0].changeTrackingEnabled", isOneOf(true, false)
+		);
+	}
+
+	@Test
+	public void testGetCTInvalidCompanyId() {
+		RestAssured.get(
+			"configurations/{companyId}", "companyId"
+		).then(
+		).statusCode(
+			HttpStatus.SC_NOT_FOUND
+		);
+	}
+
+	@Ignore
+	@Test
+	public void testPutCTNumericRequestBody() {
+		Map<String, Integer> jsonBody = new HashMap<>();
+
+		jsonBody.put("changeTrackingEnabled", 22);
+
+		RestAssured.given(
+		).body(
+			jsonBody
+		).when(
+		).contentType(
+			ContentType.JSON
+		).put(
+			"configurations/{companyId}", _companyId
+		).then(
+		).statusCode(
+			HttpStatus.SC_BAD_REQUEST
+		);
+	}
+
+	@Test
+	public void testPutCTStringRequestBody() {
+		Map<String, String> jsonBody = new HashMap<>();
+
+		jsonBody.put("changeTrackingEnabled", "22");
+
+		RestAssured.given(
+		).body(
+			jsonBody
+		).when(
+		).contentType(
+			ContentType.JSON
+		).put(
+			"configurations/{companyId}", _companyId
+		).then(
+		).statusCode(
+			HttpStatus.SC_BAD_REQUEST
+		);
+	}
+
+	@Test
+	public void testSwitchOnAndOffChangeTracking() throws PortalException {
+		RestAssured.get(
+			"configurations/{companyId}", _companyId
+		).then(
+		).statusCode(
+			HttpStatus.SC_OK
+		).body(
+			"changeTrackingEnabled", equalTo(false)
+		);
+
+		Map<String, String> jsonBody = new HashMap<>();
+
+		jsonBody.put("changeTrackingEnabled", "true");
+
+		RestAssured.given(
+		).body(
+			jsonBody
+		).when(
+		).contentType(
+			ContentType.JSON
+		).put(
+			"configurations/{companyId}", _companyId
+		).then(
+		).statusCode(
+			HttpStatus.SC_OK
+		).body(
+			"changeTrackingEnabled", equalTo(true)
+		);
+
+		jsonBody.clear();
+
+		jsonBody.put("changeTrackingEnabled", "false");
+
+		RestAssured.given(
+		).body(
+			jsonBody
+		).when(
+		).contentType(
+			ContentType.JSON
+		).put(
+			"configurations/{companyId}", _companyId
+		).then(
+		).statusCode(
+			HttpStatus.SC_OK
+		).body(
+			"changeTrackingEnabled", equalTo(false)
+		);
+	}
+
+	@Test
+	public void testUnauthenticatedRequest() {
+		RestAssured.authentication = RestAssured.preemptive(
+		).basic(
+			"", ""
+		);
+
+		RestAssured.get(
+			"configurations/{companyId}", "companyId"
+		).then(
+		).statusCode(
+			HttpStatus.SC_FORBIDDEN
+		);
+	}
+
+	private long _companyId;
+
+	@ArquillianResource
+	private URL _url;
+
+}
---
+++ modules/apps/change-tracking/change-tracking-client-test/src/testIntegration/java/com/liferay/change/tracking/client/test/internal/activator/ChangeTrackingDisabledRestTestActivator.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.client.test.internal.activator;
+
+import com.liferay.change.tracking.CTEngineManager;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class ChangeTrackingDisabledRestTestActivator
+	implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws PortalException {
+		ServiceReference<CTEngineManager> serviceReference =
+			bundleContext.getServiceReference(CTEngineManager.class);
+
+		_ctEngineManager = bundleContext.getService(serviceReference);
+
+		_ctEngineManager.disableChangeTracking(TestPropsValues.getCompanyId());
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws PortalException {
+		_ctEngineManager.disableChangeTracking(TestPropsValues.getCompanyId());
+	}
+
+	private CTEngineManager _ctEngineManager;
+
+}
