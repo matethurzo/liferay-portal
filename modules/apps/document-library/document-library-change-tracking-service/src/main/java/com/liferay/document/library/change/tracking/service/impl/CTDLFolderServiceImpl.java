2986c69508a2958f6aaa33ec7bece9153efc62a2
luiz.marins <luiz.f.marins@gmail.com>
2019-03-25 10:27:13 -0300

    LPS-90821 Restore previous code

--- modules/apps/document-library/document-library-change-tracking-service/src/main/java/com/liferay/document/library/change/tracking/service/impl/CTDLFolderServiceImpl.java
+++ modules/apps/document-library/document-library-change-tracking-service/src/main/java/com/liferay/document/library/change/tracking/service/impl/CTDLFolderServiceImpl.java
@@ -15,22 +15,24 @@
 package com.liferay.document.library.change.tracking.service.impl;
 
 import com.liferay.document.library.change.tracking.service.base.CTDLFolderServiceBaseImpl;
+import com.liferay.document.library.change.tracking.service.persistence.CTDLFolderFinderOverride;
+import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.dao.orm.QueryDefinition;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionHelper;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
- * The implementation of the ctdl folder remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.document.library.change.tracking.service.CTDLFolderService</code> interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see CTDLFolderServiceBaseImpl
+ * @author Luiz Marins
  */
 @Component(
 	property = {
@@ -41,9 +43,59 @@ import org.osgi.service.component.annotations.Component;
 )
 public class CTDLFolderServiceImpl extends CTDLFolderServiceBaseImpl {
 
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use <code>com.liferay.document.library.change.tracking.service.CTDLFolderServiceUtil</code> to access the ctdl folder remote service.
-	 */
+	@Override
+	public List<Object> getFoldersAndFileEntriesAndFileShortcuts(
+			long groupId, long folderId, String[] mimeTypes,
+			boolean includeMountFolders, QueryDefinition<?> queryDefinition)
+		throws PortalException {
+
+		if (queryDefinition.isIncludeOwner() &&
+			(queryDefinition.getOwnerUserId() != 0)) {
+
+			queryDefinition.setOwnerUserId(getUserId());
+		}
+
+		if (!ModelResourcePermissionHelper.contains(
+				_dlFolderModelResourcePermission, getPermissionChecker(),
+				groupId, folderId, ActionKeys.VIEW)) {
+
+			return Collections.emptyList();
+		}
+
+		return _dlFolderFinder.filterFindF_FE_FS_ByG_F_M_M(
+			groupId, folderId, mimeTypes, includeMountFolders, queryDefinition);
+	}
+
+	@Override
+	public int getFoldersAndFileEntriesAndFileShortcutsCount(
+			long groupId, long folderId, String[] mimeTypes,
+			boolean includeMountFolders, QueryDefinition<?> queryDefinition)
+		throws PortalException {
+
+		if (!ModelResourcePermissionHelper.contains(
+				_dlFolderModelResourcePermission, getPermissionChecker(),
+				groupId, folderId, ActionKeys.VIEW)) {
+
+			return 0;
+		}
+
+		if (queryDefinition.isIncludeOwner() &&
+			(queryDefinition.getOwnerUserId() != 0)) {
+
+			queryDefinition.setOwnerUserId(getUserId());
+		}
+
+		return _dlFolderFinder.filterCountF_FE_FS_ByG_F_M_M(
+			groupId, folderId, mimeTypes, includeMountFolders, queryDefinition);
+	}
+
+	private static volatile ModelResourcePermission<DLFolder>
+		_dlFolderModelResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				CTDLFolderServiceImpl.class, "_dlFolderModelResourcePermission",
+				DLFolder.class);
+
+	@Reference
+	private CTDLFolderFinderOverride _dlFolderFinder;
+
 }
